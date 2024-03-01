3c475810d75234f63949c13723ad5a8a71d45fda
David Swatzell <thatsstrikethree@gmail.com>
2024-03-01 14:42:23 -0600

    add all queryselectors

--- main.js
+++ main.js
@@ -1,47 +1,24 @@
-var humanWin = document.querySelector(".human-win-section");
-var gameSelectScreen = document.querySelector(".game-type-view");
-var classicGameBox = document.querySelector(".classic-game-box");
-var difficultGameBox = document.querySelector(".difficult-game-box");
-var classicGameView = document.querySelector(".classic-game-view");
-var difficultGameView = document.querySelector(".difficult-game-view");
-var computerWin = document.querySelector(".computer-win-section");
-var computerWinTally = document.querySelector(".computer-wins");
-var humanWinTally = document.querySelector(".human-wins");
-var rockFighter = document.querySelector(".rock-fighter");
-var paperFighter = document.querySelector(".paper-fighter");
-var scissorsFighter = document.querySelector(".scissors-fighter");
-var iguanaFighter = document.querySelector(".iguana-fighter");
 var alienFighter = document.querySelector(".alien-fighter");
 var changeGameButton = document.querySelector(".change-game-button");
-var chooseGameHeader = document.querySelector(".choose-game")
+var chooseGameHeader = document.querySelector(".choose-game");
+var classicGameBox = document.querySelector(".classic-game-box");
+var classicGameView = document.querySelector(".classic-game-view");
+var classicPaperFighter = document.querySelector("#classic-paper");
+var classicRockFighter = document.querySelector("#classic-rock");
+var classicScissorsFighter = document.querySelector("#classic-scissors");
+var computerChoice = document.querySelector(".computer-choice");
+var computerWin = document.querySelector(".computer-win-section");
+var computerWinTally = document.querySelector(".computer-wins");
+var difficultGameBox = document.querySelector(".difficult-game-box");
+var difficultGameView = document.querySelector(".difficult-game-view");
+var difficultPaperFighter = document.querySelector("#difficult-paper");
+var difficultRockFighter = document.querySelector("#difficult-rock");
+var difficultScissorsFighter = document.querySelector("#difficult-scissors");
+var fighterGameView = document.querySelector(".fighter-game-view");
+var gameSelectScreen = document.querySelector(".game-type-view");
+var humanWin = document.querySelector(".human-win-section");
+var humanWinTally = document.querySelector(".human-wins");
+var lizardFighter = document.querySelector(".lizard-fighter");
+var userChoice = document.querySelector(".user-choice");
 
 
-
-classicGameBox.addEventListener('click',showClassicGame)
-difficultGameBox.addEventListener('click',showDifficultGame)
-changeGameButton.addEventListener('click',changeGame)
-
-
-
-function showClassicGame(){
-    chooseGameHeader.innerText = "Choose your fighter!";
-    gameSelectScreen.classList.add("hidden");
-    classicGameView.classList.remove("hidden");
-    changeGameButton.classList.remove("hidden");
-}
-
-function showDifficultGame(){
-    chooseGameHeader.innerText = "Choose your fighter!";
-    gameSelectScreen.classList.add("hidden");
-    classicGameView.classList.remove("hidden");
-    difficultGameView.classList.remove("hidden");
-    changeGameButton.classList.remove("hidden");
-}
-
-function changeGame(){
-    chooseGameHeader.innerText = "Choose your game!";
-    gameSelectScreen.classList.remove("hidden");
-    classicGameView.classList.add("hidden");
-    difficultGameView.classList.add("hidden");
-    changeGameButton.classList.add("hidden");
-}
